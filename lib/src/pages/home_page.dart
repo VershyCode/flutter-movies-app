fefead0dfdc8e886ec2a7dc332c9023b9dc21a75
Paul G <paulsystem32@gmail.com>
2020-07-21 12:00:16 -0600

    Implementacion del swiper
    - Se implemento el swiper del tipo stack o carousel
      donde se mostraran las peliculas actuales.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hello world'),
+      appBar: AppBar(
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search), 
+            onPressed: (){}
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas()
+          ],
+        ),
+      ),
+    );
+  }
+
+ Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+          itemBuilder: (BuildContext context, int index){
+            return Image.network("http://via.placeholder.com/350x150",fit: BoxFit.cover);
+          },
+          itemCount: 3,
+          itemWidth: 200.0,
+          layout: SwiperLayout.STACK,
+          // pagination: new SwiperPagination(),
+          // control: new SwiperControl(),
+        ),
     );
   }
 }
