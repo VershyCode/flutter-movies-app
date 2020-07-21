73b5fca655489945cc05f55e93b6d62533c47a3e
Paul G <paulsystem32@gmail.com>
2020-07-21 12:11:33 -0600

    Personalizacion del Swiper
    - Se personalizo el Widget Swiper y lo independizamos.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:movies_app/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -27,20 +27,8 @@ class HomePage extends StatelessWidget {
   }
 
  Widget _swiperTarjetas() {
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-          itemBuilder: (BuildContext context, int index){
-            return Image.network("http://via.placeholder.com/350x150",fit: BoxFit.cover);
-          },
-          itemCount: 3,
-          itemWidth: 200.0,
-          layout: SwiperLayout.STACK,
-          // pagination: new SwiperPagination(),
-          // control: new SwiperControl(),
-        ),
+    return CardSwiper(
+      peliculas: [1,2,3,4,5],
     );
   }
 }
