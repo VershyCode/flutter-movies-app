be2dde5a3dd9432dacb401834a3f63b3cec0e18d
Paul G <paulsystem32@gmail.com>
2020-07-21 19:16:06 -0600

    Provider: Peiculas
    Se creo el peliculas provider donde un metodo resuelve un
    future<List<Pelicula>> y esta listo para consumirse en la app.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/src/providers/peliculas_provider.dart';
 import 'package:movies_app/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -27,6 +28,8 @@ class HomePage extends StatelessWidget {
   }
 
  Widget _swiperTarjetas() {
+   final peliculasProvider = new PeliculasProvider();
+   peliculasProvider.getEnCines();
     return CardSwiper(
       peliculas: [1,2,3,4,5],
     );
---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:movies_app/src/models/pelicula_model.dart';
+import 'package:http/http.dart' as http;
+class PeliculasProvider {
+  String _apikey    = 'fc5bc8e39511743e51140d155c104b32';
+  String _url       = 'api.themoviedb.org';
+  String _language  = 'es-ES';
+
+  Future<List<Pelicula>> getEnCines() async {
+    /// Con [Uri] construimos la url, @param1 = url @param2 = endpoint @param3 = map with new query params.
+    final url = Uri.https(_url, '3/movie/now_playing', {
+      'api_key'   : _apikey,
+      'language'  : _language
+    });
+    final response = await http.get(url); /// [response] = respuesta de la API.
+    final decodedData = json.decode(response.body); /// Convertimos la respuesta a un json.
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']); // peliculas = lista de peliculas desde el modelo.
+
+    return peliculas.items;
+  }
+}
