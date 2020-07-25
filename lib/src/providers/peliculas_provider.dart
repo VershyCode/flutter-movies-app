26e90089e3b30a84465d68f137782e147dfc1b6c
Paul G <paulsystem32@gmail.com>
2020-07-25 00:42:02 -0600

    Comenzando a utilizar los Streams
    Se comenzo a refactorizar el peliculas provider,
    para implementar el streamBuilder para poder
    paginar en la seccion de peliculas populares.

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,11 +1,24 @@
+import 'dart:async';
 import 'dart:convert';
-
 import 'package:movies_app/src/models/pelicula_model.dart';
 import 'package:http/http.dart' as http;
+
 class PeliculasProvider {
   String _apikey    = 'fc5bc8e39511743e51140d155c104b32';
   String _url       = 'api.themoviedb.org';
   String _language  = 'es-ES';
+  int _popularesPage = 0;
+  List<Pelicula> _populares = new List();
+  final _popularesStreamController = new StreamController<List<Pelicula>>.broadcast();
+  
+  // Getters
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+
+  void disposeStreams(){
+    _popularesStreamController?.close();
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final response = await http.get(url); /// [response] = respuesta de la API.
@@ -26,11 +39,15 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+    _popularesPage++;
     final url = Uri.https(_url, '3/movie/popular', {
       'api_key'   : _apikey,
-      'language'  : _language
+      'language'  : _language,
+      'page'      : _popularesPage.toString()
     });
-
-    return await _procesarRespuesta(url);
+    final resp = await _procesarRespuesta(url);
+    _populares.addAll(resp);
+    popularesSink( _populares );
+    return resp;
   }
 }
