0239c491b90b7d1df2fdbc463ef71c0ea8e99854
Paul G <paulsystem32@gmail.com>
2020-07-26 00:10:09 -0600

    SliverList dentro de CustromScrollView
    - Se agrego un sliverlist para comenzar a posicionar
      la informacion referente a la pelicula dentro del
      scrollview.
    -* Dentro del SliverList tendremos un SliverChildListDelegate
       El cual nos permite introducir una lista de widgets.
    -* En nuestro widget _posterTitulo sera un container con una row dentro
    donde pondremos a disposicion la imagen, titulo y rating de la pelicula.

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -10,6 +10,14 @@ class PeliculaDetalle extends StatelessWidget {
       body: CustomScrollView(
         slivers: <Widget>[
           _crearAppbar( pelicula ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10.0),
+                _posterTitulo(pelicula, context),
+              ]
+            ),
+          ),
         ],
       ),
     );
@@ -36,4 +44,37 @@ class PeliculaDetalle extends StatelessWidget {
       ),
     );
   }
+
+ Widget _posterTitulo(Pelicula pelicula, BuildContext context) {
+   return Container(
+     padding: EdgeInsets.symmetric(horizontal: 20.0),
+     child: Row(
+       children: <Widget>[
+         ClipRRect(
+           borderRadius: BorderRadius.circular(20.0),
+           child: Image(
+             image: NetworkImage(pelicula.getPostImg()),
+             height: 150.0,
+           ),
+         ),
+         SizedBox(width: 20.0 ),
+         Flexible(
+           child: Column(
+             crossAxisAlignment: CrossAxisAlignment.start,
+             children: <Widget>[
+               Text(pelicula.title, style: Theme.of(context).textTheme.headline6, overflow: TextOverflow.ellipsis),
+               Text(pelicula.originalTitle, style: Theme.of(context).textTheme.subtitle1, overflow: TextOverflow.ellipsis),
+               Row(
+                 children: <Widget>[
+                   Icon(Icons.star_border),
+                   Text(pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subtitle1)
+                 ],
+               )
+             ],
+           ),
+         ),
+       ],
+     ),
+   );
+ }
 }
