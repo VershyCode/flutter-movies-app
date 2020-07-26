a6791ab310becccb2d2b5f070b7d3b7b55596d61
Paul G <paulsystem32@gmail.com>
2020-07-26 00:47:55 -0600

    Provider: Actores
    - Obtenemos la lista de los actores acorde la respuesta http de la api
      _getCast los pide y los almacena en cast.actores.
      (Pendiente utilizar _getCast en PeliculaDetalle)

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:movies_app/src/models/actores_model.dart';
 import 'package:movies_app/src/models/pelicula_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -53,5 +54,16 @@ class PeliculasProvider {
     popularesSink( _populares ); // Agregamos al stream la lista de peliculas.
     _cargando = false; // Dejamos de cargar
     return resp; // Regresamos la respuesta.
+  }
+
+  Future<List<Actor>> _getCast(String peliId) async{
+    final url = Uri.https(_url, '3/movie/$peliId/credits', {
+      'api_key': _apikey,
+      'language': _language
+    });
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+    final cast = new Cast.fromJsonList(decodedData['cast']);
+    return cast.actores;
   }
 }
